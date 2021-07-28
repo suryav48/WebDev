2373ef4f02405a0e975ae0d6d2737aaf1a740971
suryav48 <83864204+suryav48@users.noreply.github.com>
2021-07-28 19:30:33 +0530

    Initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a bootcvamp that has been delayed too much</p>
+    </div>
+  );
+}
+
+export default Heading;
---
+++ src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Computer Vision</li>
+      <li>react.js bootcamp</li>
+      <li>Cyber Security</li>
+      <li>Deep Learning</li>
+      <li>Netflix clone</li>
+    </ul>
+  );
+}
+
+export default List;
